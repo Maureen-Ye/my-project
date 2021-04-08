b768d1bd5cda1052e6539e26b6326b256d879206
Maureen-Ye <807524935@qq.com>
2021-04-08 19:12:55 +0800

    frist

---
+++ src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span class="msg">part-one</span>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .msg{
+  color: green;
+}
+</style>
